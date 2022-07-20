904b7672bc2208953ee33fb9d3a6e6a8bd7c48b4
Suraj Kumar <surajkumarjupiter@gmail.com>
2022-07-20 10:47:58 +0530

    navbar done

---
+++ components/navbar.js
@@ -0,0 +1,207 @@
+let navbar = () => {
+    return `
+    <div id="nav">
+        <div id="left-nav">
+            <a href="index.html" class="logo">
+                <img src="https://www.greenhouse.io/assets/svg/logo.svg" alt="">
+            </a>
+            <ul>
+                <li>
+                    <button id="show1">Products & Solutions</button>
+                
+                </li>
+                <li>
+                    <button id="show2">Resources</button>
+                </li>
+                <li>
+                    <button id="show3">About Us</button>
+                </li>
+                <li>
+                    <button id="show4">Community</button>
+                </li>
+            </ul>
+        </div>
+        <div id="right-nav">
+            <ul>
+                <li>
+                    <a href="#">Sign In</a>
+                </li>
+                <li>
+                    <a href="#">Sign Up</a>
+                </li>
+                <li>
+                    <button class="search">
+                    
+                    </button>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div id="nav1">
+    <div id="content1">
+        <div class="left">
+            <h3>Products</h3>
+            <div class="detail">
+                <ul>
+                    <li>
+                        <a href="">Recruiting</a>
+                    </li>
+                    <li>
+                        <a href="">Onboarding</a>
+                    </li>
+                </ul>
+            </div>
+            <h3>Segments</h3>
+            <div class="detail">
+                <ul>
+                    <li>
+                        <a href="">Enterprise</a>
+                    </li>
+                    <li>
+                        <a href="">Small to midsize</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="right">
+            <h3>Solutions</h3>
+            <div class="detail">
+                <ul>
+                    <li>
+                        <a href="">Integrations
+                        </a>
+                        
+                    </li>
+                    <li>
+                        <a href="">Structured hiring</a>
+                    </li>
+                    <li>
+                        <a href="">Talent sourcing</a>
+                    </li>
+                    <li>
+                        <a href="">Candidate experience
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">Diversity, equity & inclusion</a>
+                    </li>
+                    <li>
+                        <a href="">More solutions</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div id="nav2">
+    <div id="content2">
+        <div class="left1">
+            <div class="detail">
+                <ul>
+                    <li>
+                        <a href="">Blog</a>
+                    </li>
+                    <li>
+                        <a href="">Guidance</a>
+                    </li>
+                    <li>
+                        <a href="">Customer stories</a>
+                    </li>
+                    <li>
+                        <a href="">Hiring glossary</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="right1">
+            <div>
+                <img src="https://sharp.services.greenhouse.io/production/Talent-Makers-book-cover-n.png?auto=format&fit=max&lossless=true&q=90&w=426&s=39652d73de16c7719a700214bce70d443705da7a6050c920b1c5a9cdbad2d60a" alt="">
+                
+            </div>
+            <div class="avail">
+                <p>Now Available</p>
+                <h3>Talent Makers – The definitive book on great</h3>
+                <h4>We’ve packed all our insights, strategies and tips into a book that will help you make hiring your superpower.</h4>
+                <a href="">See the reviews</a>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div id="nav3">
+    <div id="content3">
+        <div class="left1">
+            <div class="detail">
+                <ul>
+                    <li>
+                        <a href="">Mission</a>
+                    </li>
+                    <li>
+                        <a href="">Belonging</a>
+                    </li>
+                    <li>
+                        <a href="">Company</a>
+                    </li>
+                    <li>
+                        <a href="">Careers</a>
+                    </li>
+                    <li>
+                        <a href="">Press & awards</a>
+                    </li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+                
+            </div>
+        </div>
+        <div class="right1">
+            <div>
+                <img src="https://sharp.services.greenhouse.io/production/Greenhouse-Pride-tshirt-n.jpg?auto=format&fit=max&lossless=true&q=90&w=426&s=de44e24806b388901684135be6acb800917d827f4b3424eade2f1300c44d2177" alt="">
+            </div>
+            <div class="avail">
+                <p>Greenhouse celebrates Pride</p>
+                <h3>Buy a shirt, help an LGBTQ+ teen</h3>
+                <h4>We’re shining a spotlight on the LGBTQ+ community with our new Pride t-shirt. 100% of the proceeds are going to support The Trevor Project and the Ali Forney Center.</h4>
+                <a href="">Get your Pride shirt</a>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div id="nav4">
+    <div id="content4">
+        <div class="left1">
+            <div class="detail">
+                <ul>
+                    <li>
+                        <a href="">Events</a>
+                    </li>
+                    <li>
+                        <a href="">Open</a>
+                    </li>
+                    <li>
+                        <a href="">Talent Makers</a>
+                    </li>
+                    <li>
+                        <a href="">Newsletter</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="right1">
+            <div>
+                <img src="https://sharp.services.greenhouse.io/production/Photo-of-a-woman-wearing-a-headdress-n.png?auto=format&fit=max&lossless=true&q=90&w=426&s=efbf30b0032415c27114661976fb1308ac70708cc11d1f5ceca5c4f67344fbef" alt="">
+            </div>
+            <div class="avail">
+                <p>Open Conference 2022</p>
+                <h3>Hire for what’s next</h3>
+                <h4>Join us in person or virtually at the new Jacob Javits Center for a two-day conference designed to elevate your hiring in real time, from May 24–25, 2022.</h4>
+                <a href="">Learn More</a>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    `
+}
+
+
+
+export default navbar 
