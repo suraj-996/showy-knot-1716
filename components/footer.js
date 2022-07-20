6a7ae6d3e141f01e7bd8805f846a22c78891eb16
Suraj Kumar <surajkumarjupiter@gmail.com>
2022-07-20 20:22:56 +0530

    footer

---
+++ components/footer.js
@@ -0,0 +1,147 @@
+let footer = () => {
+    return `
+    <section id="section">
+    <div>
+      <div>
+        <h2>Ready to become great at hiring?</h2>
+        <a href="">Request a demo</a>
+      </div>
+      <div>
+        <img
+          src="https://www.greenhouse.io/assets/img/botanical/prefooter-image3.png"
+          alt=""
+        />
+      </div>
+    </div>
+  </section>
+  <footer id="footer">
+    <div id="container">
+      <div class="content">
+        <h3>Products</h3>
+        <ul>
+          <li><a href="">Recruiting</a></li>
+          <li><a href="">Onboarding</a></li>
+          <li><a href="">Pricing</a></li>
+        </ul>
+        <h3>Segments</h3>
+        <ul>
+          <li><a href="">Enterprise</a></li>
+          <li><a href="">Small to midsize</a></li>
+        </ul>
+        <h3>Solutions</h3>
+        <ul>
+          <li><a href="">Integrations</a></li>
+          <li><a href="">Structured hiring</a></li>
+          <li><a href="">Talent sourcing</a></li>
+          <li><a href="">Candidate experience</a></li>
+          <li><a href="">Diversity, equity & inclusion</a></li>
+          <li><a href="">More solutions</a></li>
+        </ul>
+        <div class="social">
+          <a href="https://www.facebook.com/greenhouse.io/">
+            <img
+              src="https://sharp.services.greenhouse.io/production/Facebook_200116_235458.svg?auto=compress&fit=max&lossless=true&q=90&w=60&s=38b1bf80b0469b5ee7a23e86aaa857c0b3dc2211419ad3c3cbca641ad934fbbb"
+              alt=""
+            />
+          </a>
+          <a href="https://www.linkedin.com/company/greenhouse-inc-/">
+            <img
+              src="https://sharp.services.greenhouse.io/production/linkedin.svg?auto=compress&fit=max&lossless=true&q=90&w=60&s=34b956c8a836733a8ea153b0c863dc2ba794b55da68089e84562f4195d22367d"
+              alt=""
+            />
+          </a>
+          <a href="https://twitter.com/Greenhouse"
+            ><img
+              src="https://sharp.services.greenhouse.io/production/twitter.svg?auto=compress&fit=max&lossless=true&q=90&w=60&s=4e1f4d71063dfc1bbd756ebaba43b29ad3c3684ac187e2de47e2c2f41290cd92"
+              alt=""
+          /></a>
+          <a href="https://www.youtube.com/channel/UCwLlhrx5lFssgTwKCHEuC8g"
+            ><img
+              src="https://sharp.services.greenhouse.io/production/youtube.svg?auto=compress&fit=max&lossless=true&q=90&w=60&s=a3e493c512f90d2bcb6bd7f8629208b0adc0dcf405cc044f89bc8352fd39e62c"
+              alt=""
+          /></a>
+        </div>
+      </div>
+      <div class="content">
+        <h3>Resources</h3>
+        <ul>
+          <li><a href="">Blog</a></li>
+          <li><a href="">Guidance</a></li>
+          <li><a href="">Customer stories</a></li>
+          <li><a href="">Hiring glossary</a></li>
+          <li><a href="">Support</a></li>
+          <li><a href="">Developer resources</a></li>
+        </ul>
+        <h3>Platform</h3>
+        <ul>
+          <li><a href="">Ethical principles</a></li>
+          <li><a href="">Services</a></li>
+          <li><a href="">Mobile</a></li>
+          <li><a href="">APIs</a></li>
+          <li><a href="">Security</a></li>
+          <li><a href="">GDPR</a></li>
+        </ul>
+      </div>
+      <div class="content">
+        <h3>About Us</h3>
+        <ul>
+          <li><a href="">Mission</a></li>
+          <li><a href="">Belonging</a></li>
+          <li><a href="">Company</a></li>
+          <li><a href="">Careers</a></li>
+          <li><a href="">Press & awards</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+        <h3>Community</h3>
+        <ul>
+          <li><a href="">Events</a></li>
+          <li><a href="">Open</a></li>
+          <li><a href="">Talent Makers</a></li>
+          <li><a href="">Newsletter</a></li>
+        </ul>
+      </div>
+      <div class="content">
+        <h2>Modern Recruiter newsletter</h2>
+        <p>
+          Our bi-weekly newsletter full of inspiration, podcasts, trends and
+          news.
+        </p>
+        <div class="email">
+          <label for="email">Email address</label>
+          <input type="email" id="email" />
+        </div>
+        <button>Subscribe</button>
+        <div class="select">
+          <label for="lan">Choose your language</label>
+          <select name="" id="">
+            <option value="us">English (US)</option>
+            <option value="uk">English (UK)</option>
+            <option value="">Deutsch</option>
+          </select>
+        </div>
+      </div>
+    </div>
+    <div id="copyright">
+      <p>
+        ©2022 Greenhouse Software, Inc.<br>
+        “Hire for what’s next” and “Greenhouse Talent Makers” are trademarks of Greenhouse&nbsp;Software.
+      </p>
+      <div class="links">
+          <ul>
+            <li>
+              <a href="">Cookie preferences</a>
+            </li>
+            <li>
+              <a href="https://www.greenhouse.io/privacy-policy">Privacy policy</a>
+            </li>
+            <li>
+              <a href="https://www.greenhouse.io/legal">Terms of service</a>
+            </li>
+          </ul>
+      </div>
+    </div>
+  </footer>
+    `
+}
+
+export default footer
